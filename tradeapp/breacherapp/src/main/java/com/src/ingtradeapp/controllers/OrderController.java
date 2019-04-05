c2b81e8a4b01cc35f38d0aeff44039d5bddfae15
Prakash <prakashooty16@gmail.com>
2019-04-05 13:34:03 +0530

    For Order Controller

--- tradeapp/breacherapp/src/main/java/com/src/ingtradeapp/controllers/OrderController.java
+++ tradeapp/breacherapp/src/main/java/com/src/ingtradeapp/controllers/OrderController.java
@@ -1,27 +1,34 @@
 package com.src.ingtradeapp.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.src.ingtradeapp.model.JSONResponse;
+import com.src.ingtradeapp.model.Orders;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+
+	@Autowired
+	JSONResponse response;
+	
 	@GetMapping("")
-	public String getOrders() {
-		return "{ \r\n" + 
-				"  \"id\":1,\r\n" + 
-				"  \"stock_name\":\"Test 123\",\r\n" + 
-				"  \"stock_price\":120,\r\n" + 
-				"  \"volume\":1,\r\n" + 
-				"  \"trade_time\":\"2018-02-03 05:34+GMT\",\r\n" + 
-				"  \"fees\":12\r\n" + 
-				"}";
+	public List<Orders> getOrders() {
+		return new ArrayList();
 	}
 
 	@PostMapping("")
-	public String PlaceOrders() {
-		return "{\r\n" + "  \"id\":1,\r\n" + "  \"name\":\"rte\"\r\n" + "}";
+	public JSONResponse placeOrders(@RequestBody Orders order) {
+		response.setMsg("Added Succesfully");
+		response.setStatus(true);
+		return response;
 	}
 }
--- tradeapp/breacherapp/src/main/java/com/src/ingtradeapp/model/JSONResponse.java
+++ tradeapp/breacherapp/src/main/java/com/src/ingtradeapp/model/JSONResponse.java
@@ -4,6 +4,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class JSONResponse {
 	private String msg;
+	private Boolean status;
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
 
 	public String getMsg() {
 		return msg;
